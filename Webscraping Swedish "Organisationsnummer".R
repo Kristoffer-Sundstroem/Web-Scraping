52e4f9ab05fc5177732b54588f8e04f48a81cc38
Kristoffer Sundström <109088867+Kristoffer-Sundstroem@users.noreply.github.com>
2024-12-10 13:31:46 +0100

    Update Webscraping Swedish "Organisationsnummer".R

--- Webscraping Swedish "Organisationsnummer".R
+++ Webscraping Swedish "Organisationsnummer".R
@@ -3,7 +3,7 @@ searchname <- paste0(websearch,
                      URLencode(firm_list)) # Create a list that is a combination of the websearch and the URLencoded firm names (taken from firm_list).
 
 for (x in 1:length(firm_list) { # Loop the scraping script over the number of firms in the firm_list.
-  message("Retrieving OrgNr ", x, ".") # This line give the user information on how far it has come.
+  message("Retrieving OrgNr ", x, ".") # This line give the user information on how far the code has come in terms of collected "organisationsnummer".
   tmp_doc <- read_html(searchname[x]) # Create a temporary variable called "tmp_doc".
   tmp_html_prod <- tmp_doc %>% # This part makes use of dplyr and it retrieves the "span" element of the html information saved in tmp_doc and then it collects the text from the new temporay file "tmp_html_prod".
     html_elements("span") %>%
@@ -16,11 +16,11 @@ for (x in 1:length(firm_list) { # Loop the scraping script over the number of fi
   
   rm(tmp_doc, tmp_html_prod, tmp_nr)
   
-  if((x %% 100) == 0) {
-    message("Taking a break.")
-    message("Number of retrieved OrgNrs ", x, ".")
-    Sys.sleep(60)
+  if((x %% 100) == 0) { # This part specifies that this specific part (uner the if command) shall run after the loop has reached the 100th iteration.
+    message("Taking a break.") # Gives the user information about the fact that the scraping script is taking a break.
+    message("Number of retrieved OrgNrs ", x, ".") # Informs the user about the number of retrieved "organisationsnummer".
+    Sys.sleep(60) # This forces the script to take a 60 second break in order to not overload the servers.
   }
 }
 
-EnvDataSet_Tibble$OrgNr <- sub("^\\D+", "", EnvDataSet_Tibble$OrgNr)
+# EnvDataSet_Tibble$OrgNr <- sub("^\\D+", "", EnvDataSet_Tibble$OrgNr)
